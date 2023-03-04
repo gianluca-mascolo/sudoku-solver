7a730ccecf59d5b4c756fd7cedee5a4bbc5178b7
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-03-05 00:36:10 +0100

    change sudoku repr struct

--- sudoku_solver/sudoku.py
+++ sudoku_solver/sudoku.py
@@ -2,143 +2,51 @@
 class SudokuBoard:
     def __init__(self):
         self.cell = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-        self.board = [
-            [
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-            ],
-            [
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-            ],
-            [
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-            ],
-            [
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-            ],
-            [
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-            ],
-            [
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-            ],
-            [
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-            ],
-            [
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-            ],
-            [
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-                self.cell,
-            ],
-        ]
+        self.board = [self.cell for x in range(81)]
 
     def full(self):
-        uniqueSets = 0
-        for y in range(9):
-            for x in range(9):
-                if len(self.board[y][x]) == 1:
-                    uniqueSets += 1
-        if uniqueSets == 81:
-            return True
-        else:
-            return False
+        board_len = sum(list(map(lambda x: len(x), self.board)))
+        return board_len == 81
 
     def valid(self):
-        for y in range(9):
-            for x in range(9):
-                if self.board[y][x] == set():
-                    print("set not found")
-                    return False
-                if str(next(iter(self.board[y][x]))) not in self.cell:
-                    print("Invalid cell")
-                    return False
-                if len(self.board[y][x]) == 1:
-                    for dx in range(9):
-                        if dx != x and self.board[y][x] == self.board[y][dx]:
-                            print("Duplicate in line")
+        for pos, element in enumerate(self.board):
+            if element == set():
+                print("Cell is empty")
+                return False
+            if element.issubset(self.cell) is False:
+                print("Invalid cell content: {0}".format(element))
+                return False
+            if len(element) == 1:
+                for x in range(9 * (pos // 9), 9 * (pos // 9) + 9):
+                    if x != pos:
+                        if self.board[x] == element:
+                            print(
+                                "Duplicate in line. Board {0} and Board {1} equal {2}".format(
+                                    x, pos, element
+                                )
+                            )
                             return False
-                    for dy in range(9):
-                        if dy != y and self.board[y][x] == self.board[dy][x]:
-                            print("Duplicate in column")
+                for x in range(pos % 9, 81, 9):
+                    if x != pos:
+                        if self.board[x] == element:
+                            print(
+                                "Duplicate in Column. Board {0} and Board {1} equal {2}".format(
+                                    x, pos, element
+                                )
+                            )
                             return False
-                    for dy in range(y - y % 3, y + (3 - y % 3)):
-                        for dx in range(x - x % 3, x + (3 - x % 3)):
-                            if (dy != y or dx != x) and self.board[dy][
-                                dx
-                            ] == self.board[y][x]:
-                                print("Duplicate in square")
+                for x in range(81):
+                    if (
+                        3 * (x // 9 // 3) + x // 3 % 3
+                        == 3 * (pos // 9 // 3) + pos // 3 % 3
+                    ):
+                        if x != pos:
+                            if self.board[x] == element:
+                                print(
+                                    "Duplicate in Square. Board {0} and Board {1} equal {2}".format(
+                                        x, pos, element
+                                    )
+                                )
                                 return False
         return True
 
@@ -157,8 +65,15 @@ def load_sudoku(sudoku_file: str, sudoku: SudokuBoard):
 
 def main():
     sudoku = SudokuBoard()
+    sudoku.board[0] = {"3"}
+    sudoku.board[9] = {"3"}
+    # print(sudoku.full())
     print(sudoku.valid())
 
 
 if __name__ == "__main__":
     main()
+
+
+# for x in range(81):
+#    print("x: {0} r: {1}, q: {2} t: {3}".format(x,x//9//3,x//3%3,3*(x//9//3)+x//3%3))
