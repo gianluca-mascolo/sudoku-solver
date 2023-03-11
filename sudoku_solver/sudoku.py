498f9d7c2e63eef6ecbbc30f5f42fec61942a6c0
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-03-11 15:42:30 +0100

    map reduce and combine

--- sudoku_solver/sudoku.py
+++ sudoku_solver/sudoku.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-import copy
-from enum import Enum
-
-
-class SudokuGroup(Enum):
-    LINE, COLUMN, SQUARE, ALL = range(4)
+# import copy
+from functools import reduce
+from itertools import combinations
 
 
 class SudokuBoard:
@@ -23,32 +20,26 @@ class SudokuBoard:
             if element.issubset({"1", "2", "3", "4", "5", "6", "7", "8", "9"}) is False:
                 print("Invalid cell. position: {0} content: {1}".format(pos, element))
                 return False
-            if self.is_unique(pos):
-                if element in [self.board[x] for x in neighbor(pos=pos, group=SudokuGroup.ALL)]:
+            if len(element) == 1:
+                if element in [self.board[x] for x in neighbor(pos=pos)["all"]]:
                     print("Find duplicate {0}".format(element))
                     return False
         return True
 
-    def list_unique(self, exclude=[]):
-        return [x for x in range(81) if len(self.board[x]) == 1 and x not in exclude]
-
-    def list_multiple(self, exclude=[]):
-        return [x for x in range(81) if len(self.board[x]) > 1 and x not in exclude]
-
-    def is_unique(self, pos: int):
-        if len(self.board[pos]) == 1:
-            return str(next(iter(self.board[pos])))
+    def list_elements(self, multiple):
+        if multiple:
+            return [{"position": x, "element": self.board[x]} for x in range(81) if len(self.board[x]) > 1]
         else:
-            return False
+            return [{"position": x, "element": self.board[x]} for x in range(81) if len(self.board[x]) == 1]
 
     def list_twins(self, exclude=[]):
         twins = []
         twins_checked = set()
         for pos in range(81):
             if len(self.board[pos]) == 2:
-                for x in neighbor(pos=pos, group=SudokuGroup.ALL):
+                for x in neighbor(pos=pos)["all"]:
                     if self.board[pos] == self.board[x] and x not in twins_checked:
-                        twins.append({"position": [pos, x], "value": self.board[pos]})
+                        twins.append({"position": [pos, x], "element": self.board[pos]})
                         twins_checked.add(pos)
                         twins_checked.add(x)
         return twins
@@ -63,18 +54,26 @@ def load_sudoku(sudoku_file: str, sudoku: SudokuBoard):
     return sudoku.valid()
 
 
-def neighbor(pos: int, group: SudokuGroup, exclude=[]):
+def neighbor(pos: int):
     line = [x for x in range(9 * (pos // 9), 9 * (pos // 9) + 9) if x != pos]
     column = [x for x in range(pos % 9, 81, 9) if x != pos]
     square = [x for x in range(81) if 3 * (x // 9 // 3) + x // 3 % 3 == 3 * (pos // 9 // 3) + pos // 3 % 3 and x != pos]
-    if group == SudokuGroup.ALL:
-        return list(set(line + column + square) - set(exclude))
-    elif group == SudokuGroup.LINE:
-        return line
-    elif group == SudokuGroup.COLUMN:
-        return column
-    elif group == SudokuGroup.SQUARE:
-        return square
+    return {
+        "line": line,
+        "column": column,
+        "square": square,
+        "all": list(set(line + column + square)),
+    }
+    # return {
+    #     "line": [x for x in line if x not in exclude],
+    #     "column": [x for x in column if x not in exclude],
+    #     "square": [x for x in square if x not in exclude],
+    #     "all": list(set(line + column + square) - set(exclude)),
+    # }
+
+
+def is_alone(pos: int, sudoku: SudokuBoard):
+    return True
 
 
 def twin_neighbor(t1: int, t2: int):
@@ -92,7 +91,7 @@ def twin_neighbor(t1: int, t2: int):
 
 
 def print_sudoku(sudoku: SudokuBoard):
-    unique = sudoku.list_unique()
+    unique_numbers = sudoku.list_elements(multiple=False)
     print("+---+---+---+")
     for p in range(81):
         if p % 3 == 0:
@@ -103,7 +102,7 @@ def print_sudoku(sudoku: SudokuBoard):
                 print("|", end="")
             else:
                 print("|", end="")
-        if p in unique:
+        if p in [x["position"] for x in unique_numbers]:
             print(str(next(iter(sudoku.board[p]))), end="")
         else:
             print("_", end="")
@@ -116,23 +115,20 @@ def print_sudoku(sudoku: SudokuBoard):
 def solve_sudoku(sudoku: SudokuBoard):
     print("solving sudoku")
     checked = set()
-    while sudoku.valid() and sudoku.full() is False and len(sudoku.list_unique()) > len(checked):
-        print(f"Total unique: {len(sudoku.list_unique())} {len(checked)}")
-        for x in sudoku.list_unique(exclude=list(checked)):
-            for p in neighbor(pos=x, group=SudokuGroup.ALL, exclude=sudoku.list_unique()):
-                sudoku.board[p].discard(sudoku.is_unique(x))
-                checked.add(x)
-        for x in sudoku.list_multiple():
-            for v in sudoku.board[x]:
-                if sum(list(map(lambda i: v not in sudoku.board[i], neighbor(x, SudokuGroup.LINE)))) == 8:
-                    sudoku.board[x] = {v}
-                if sum(list(map(lambda i: v not in sudoku.board[i], neighbor(x, SudokuGroup.COLUMN)))) == 8:
-                    sudoku.board[x] = {v}
-                if sum(list(map(lambda i: v not in sudoku.board[i], neighbor(x, SudokuGroup.SQUARE)))) == 8:
-                    sudoku.board[x] = {v}
-        for twin in sudoku.list_twins():
-            for p in twin_neighbor(twin["position"][0], twin["position"][1]):
-                sudoku.board[p].difference_update(twin["value"])
+    while sudoku.valid() and sudoku.full() is False and len(sudoku.list_elements(multiple=False)) > len(checked):
+        for position, element in filter(lambda x: len(x[1]) == 1, enumerate(sudoku.board)):
+            for p in neighbor(position)["all"]:
+                sudoku.board[p] -= element
+                checked.add(position)
+        for position, element in filter(lambda x: len(x[1]) > 1, enumerate(sudoku.board)):
+            for group in ["line", "square", "column"]:
+                if alone := element - reduce(lambda a, b: a | b, map(lambda x: sudoku.board[x], neighbor(position)[group])):
+                    sudoku.board[position] = alone
+        for position, element in (twins := list(filter(lambda x: len(x[1]) == 2, enumerate(sudoku.board)))):
+            twins_position = set([x[0] for x in twins if x[1] == element])
+            for combo in combinations(twins_position, 2):
+                for p in twin_neighbor(combo[0], combo[1]):
+                    sudoku.board[p].difference_update(element)
         print_sudoku(sudoku)
     return sudoku.valid()
 
@@ -140,19 +136,23 @@ def solve_sudoku(sudoku: SudokuBoard):
 def main():
     sudoku = SudokuBoard()
     load_sudoku("sudoku2.txt", sudoku)
+    print_sudoku(sudoku)
     solve_sudoku(sudoku)
-    while sudoku.valid() and sudoku.full() is False:
-        for twin in sudoku.list_twins():
-            for v in twin["value"]:
-                trydoku = copy.deepcopy(sudoku.board)
-                sudoku.board[twin["position"][0]] = {v}
-                r = solve_sudoku(sudoku)
-                if r is False:
-                    sudoku.board = copy.deepcopy(trydoku)
-                    print(sudoku.board[twin["position"][0]])
-                    print(f"Try: {r}")
-            print(f"sudoku is valid: {sudoku.valid()}")
-            print_sudoku(sudoku)
+    print(sudoku.valid())
+    for v in [{"position": x, "element": sudoku.board[x]} for x in range(81) if len(sudoku.board[x]) > 1]:
+        print(v)
+    # while sudoku.valid() and sudoku.full() is False:
+    #     for twin in sudoku.list_twins():
+    #         for v in twin["value"]:
+    #             trydoku = copy.deepcopy(sudoku.board)
+    #             sudoku.board[twin["position"][0]] = {v}
+    #             r = solve_sudoku(sudoku)
+    #             if r is False:
+    #                 sudoku.board = copy.deepcopy(trydoku)
+    #                 print(sudoku.board[twin["position"][0]])
+    #                 print(f"Try: {r}")
+    #         print(f"sudoku is valid: {sudoku.valid()}")
+    #         print_sudoku(sudoku)
 
 
 if __name__ == "__main__":
