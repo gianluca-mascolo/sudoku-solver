143a23b925a988cffccf9261d1fe5afc0b499b05
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-03-18 20:07:53 +0100

    experiments

--- sudoku_solver/sutui.py
+++ sudoku_solver/sutui.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python3
-from textual.app import App, ComposeResult, RenderResult
-from textual.widget import Widget
+from textual.app import App, ComposeResult
+from textual.widgets import Static
 
 
-class SudokuCell(Widget):
-    """Display a sudoku input cell."""
+class SudokuCell(Static):
+    """Display Sudoku cell."""
 
-    def render(self) -> RenderResult:
-        return "[b]X[/b]"
+    # def __init__(self, *args, **kwargs):
+    #     self.id=1
+    #     self.name="xx"
+    # #pos = self.id
+    # #pos = name
+    DEFAULT_CSS = """
+    SudokuCell {{
+        height: {0};
+    }}
+    """.format(
+        "1"
+    )
+
+    def on_mount(self) -> None:
+        self.render()
 
 
 class SudokuApp(App):
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+    """
+
     def compose(self) -> ComposeResult:
-        yield SudokuCell()
+        for x in range(2):
+            yield SudokuCell(id=f"cell{x}", renderable="X", name=f"{x}")
 
 
 def main():
