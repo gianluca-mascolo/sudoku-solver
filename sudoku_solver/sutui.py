d3ccf8acce376985a7be54227c539698db23820a
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-03-19 16:59:49 +0100

    grid with inputs

--- sudoku_solver/sutui.py
+++ sudoku_solver/sutui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+from textual import events
 from textual.app import App, ComposeResult
-from textual.widgets import Static
+from textual.widgets import Footer, Header, Static
 
 
 class SudokuCell(Static):
@@ -15,22 +16,68 @@ class SudokuApp(App):
     Screen {
         align: left top;
         layout: grid;
-        grid-size: 3 3;
-        grid-gutter: 1;
-        grid-rows: 1fr;
-        grid-columns: 1fr;
+        grid-size: 9 10;
+        grid-gutter: 0 0;
+        grid-rows: 2;
+        grid-columns: 3;
+        padding: 0;
     }
     .cell {
-    height: 3;
+    height: 2;
     width: 3;
-    border: solid green;
+    border-left: solid blue;
+    border-top: solid blue;
+    }
+    .selected {
+    height: 2;
+    width: 3;
+    border-left: solid yellow;
+    border-top: solid yellow;
+    }
+    #filler {
+    column-span: 9;
+    height: 100%;
+    width: 100%;
+    content-align: center middle;
+    border: none
     }
     """
-    board = list(range(9))
+    BINDINGS = [("s,S", "solve", "Solve"), ("c,C", "clear", "Clear"), ("q,Q", "quit", "Quit")]
+    board = list(map(lambda x: x % 9 + 1, range(81)))
+    position = 0
+    sudoku_grid = [SudokuCell(id=f"cell{p}", renderable=f"{v}", name=f"{p}", classes="cell") for p, v in enumerate(board)]
+    sudoku_grid[0].add_class("selected")
 
     def compose(self) -> ComposeResult:
-        for p, v in enumerate(self.board):
-            yield SudokuCell(id=f"cell{p}", renderable=f"{v}", name=f"{p}", classes="cell")
+        yield Header(show_clock=True)
+        yield Footer()
+
+        for p in range(81):
+            yield self.sudoku_grid[p]
+        yield Static("", classes="cell", id="filler")
+
+    def on_key(self, event: events.Key) -> None:
+        key = event.key
+        p = self.position
+        board = self.board
+        if key in list(map(lambda c: str(c), range(1, 9))):
+            board[p] = key
+        else:
+            board[p] = ""
+        self.sudoku_grid[p].remove_class("selected")
+        self.sudoku_grid[p].update(board[p])
+        self.position = (p + 1) % 81
+        self.sudoku_grid[self.position].add_class("selected")
+        return True
+
+    def action_quit(self) -> None:
+        self.app.exit()
+
+    def action_solve(self) -> None:
+        return True
+
+    def action_clear(self) -> None:
+        return True
 
 
 def main():
