23aae9ab577e0d22d5a8005bcefb2415091765a4
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-03-20 08:37:48 +0100

    global grid

--- sudoku_solver/sudoku.py
+++ sudoku_solver/sudoku.py
@@ -6,6 +6,7 @@ from textual.widgets import Footer, Header, Static
 from sudoku_solver.sudokulib import SudokuBoard, cursedoku, solve_sudoku
 
 position = 0
+sudoku_grid = []
 
 
 class SudokuCell(Static):
@@ -15,7 +16,9 @@ class SudokuCell(Static):
         self.render()
 
     def on_click(self) -> None:
+        global sudoku_grid
         global position
+        sudoku_grid[position].remove_class("selected")
         self.add_class("selected")
         position = int(self.name)
         # self.position=int(self.name)
@@ -54,7 +57,6 @@ class SudokuApp(App):
     """
     BINDINGS = [("s,S", "solve", "Solve"), ("c,C", "clear", "Clear"), ("q,Q", "quit", "Quit")]
     sudoku = SudokuBoard()
-    sudoku_grid = []
     for p, v in enumerate(sudoku.board):
         if len(v) == 1:
             sudoku_grid.append(SudokuCell(id=f"cell{p}", renderable=f"{list(v)[0]}", name=f"{p}", classes="cell"))
@@ -63,30 +65,32 @@ class SudokuApp(App):
     sudoku_grid[0].add_class("selected")
 
     def compose(self) -> ComposeResult:
+        global sudoku_grid
         yield Header(show_clock=True)
         yield Footer()
 
         for p in range(81):
-            yield self.sudoku_grid[p]
+            yield sudoku_grid[p]
         yield Static("", classes="cell", id="filler")
 
     def on_key(self, event: events.Key) -> None:
+        global sudoku_grid
         global position
         key = event.key
         currentvalue = self.sudoku.board[position]
-        self.sudoku_grid[position].remove_class("selected")
+        sudoku_grid[position].remove_class("selected")
         if key in list(map(lambda c: str(c), range(1, 10))):
             self.sudoku.board[position] = {key}
             if self.sudoku.valid():
-                self.sudoku_grid[position].update(key)
+                sudoku_grid[position].update(key)
                 position = (position + 1) % 81
             else:
                 self.sudoku.board[position] = currentvalue
         elif key == "x" or key == "X":
             self.sudoku.board[position] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-            self.sudoku_grid[position].update("")
+            sudoku_grid[position].update("")
             position = (position + 1) % 81
-        self.sudoku_grid[position].add_class("selected")
+        sudoku_grid[position].add_class("selected")
 
     def action_quit(self) -> None:
         self.app.exit()
@@ -99,6 +103,7 @@ class SudokuApp(App):
                 self.sudoku_grid[p].update(str(list(v)[0]))
 
     def action_clear(self) -> None:
+        global sudoku_grid
         global position
         for x in range(81):
             self.sudoku.board[x] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
