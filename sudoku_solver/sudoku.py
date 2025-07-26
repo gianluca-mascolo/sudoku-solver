d0d90965a9bb0a90bdcbd25d0c12be4a037723d5
Gianluca Mascolo <gmascolo@influxdata.com>
2025-07-26 16:43:42 +0200

    bump textual to 0.89

--- sudoku_solver/sudoku.py
+++ sudoku_solver/sudoku.py
@@ -90,15 +90,14 @@ class SudokuApp(App):
     height: 100%;
     }
     """
-    BINDINGS = [("s,S", "solve", "Solve"), ("c,C", "clear", "Clear"), ("l,L", "push_screen('sudokufile')", "Load"), ("q,Q", "quit", "Quit")]
+    BINDINGS = [("s,S", "solve", "Solve"), ("c,C", "clear", "Clear"), ("l,L", "load_file", "Load"), ("q,Q", "quit", "Quit")]
     sudoku = SudokuBoard()
-    SCREENS = {"sudokufile": SudokuFile(sudoku=sudoku)}
 
     for p, v in enumerate(sudoku.board):
         if len(v) == 1:
-            sudoku_grid.append(SudokuCell(id=f"cell{p}", renderable=f"{list(v)[0]}", name=f"{p}", classes="cell"))
+            sudoku_grid.append(SudokuCell(f"{list(v)[0]}", id=f"cell{p}", name=f"{p}", classes="cell"))
         else:
-            sudoku_grid.append(SudokuCell(id=f"cell{p}", renderable="", name=f"{p}", classes="cell"))
+            sudoku_grid.append(SudokuCell("", id=f"cell{p}", name=f"{p}", classes="cell"))
     sudoku_grid[0].add_class("selected")
     message = Static("'1-9' insert, 'x' delete", classes="cell", id="filler")
 
@@ -153,6 +152,9 @@ class SudokuApp(App):
         position = 0
         sudoku_grid[0].add_class("selected")
         self.message.update("'1-9' insert, 'x' delete")
+
+    def action_load_file(self) -> None:
+        self.push_screen(SudokuFile(self.sudoku))
 
 
 def main():
